8a75d1644412863996824632952a14ed6868f9b9
PilotChalkanov <nik.chalkanov@gmail.com>
2024-11-13 10:12:46 +0200

    add vars to root

--- tf-mod4-lab/provider.tf
+++ tf-mod4-lab/provider.tf
@@ -2,6 +2,6 @@ provider "google" {
   credentials = file("/home/niko/UNI/Networks/CloudNetworking/npp-cloud/onyx-outpost-438905-q4-ad8b6c690641.json")
 
   project = "onyx-outpost-438905-q4"
-  region  = "europe-west1"  // default
+  region  = var.region  // default
   zone    = "europe-west1-c"  // default
 }
